ac198d782c5fb342ca92634308e8effceab81d65
Alexdm17 <alexis.dacalmarien@gmail.com>
2025-07-23 17:37:29 +0200

    feat: integrate react-use for debouncing search term in movie fetch

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { useDebounce } from "react-use";
 
 import { Spinner } from "./components/Spinner.jsx";
 import { Search } from "./components/Search.jsx";
@@ -22,6 +23,9 @@ function App() {
   const [errorMessage, setErrorMessage] = useState(null);
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
+
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
 
   const fetchMovies = async (query = "") => {
     setIsLoading(true);
@@ -55,8 +59,8 @@ function App() {
   };
 
   useEffect(() => {
-    fetchMovies(searchTerm);
-  }, [searchTerm]);
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
 
   return (
     <>
